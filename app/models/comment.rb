be3e8cafe9da5c7b2d470e1183e5d0c7fa0b1eb8
Michele Iacobone <michele.iacobone@gmail.com>
2014-09-07 02:47:36 +0200

    Add validations for posts and comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+
+  #additional Validations: every post must have a post_id (which links it to the corresponding post) and a body
+  validates_presence_of :post_id
+  validates_presence_of :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy #when a Post is deleted, we want all Comments associated with it to be deleted as well.
+
+  #additional Validations: every post must have a title and a body
+  validates_presence_of :title
+  validates_presence_of :body
+
 end
